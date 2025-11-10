35b040ee7b5a7383da90a9a685258461654a473f
Eden Lange <elange017@berkeley.edu>
2025-11-10 04:10:42 +0000

    edits made to notebook and utils.py to be compatible

--- ligotools/.ipynb_checkpoints/utils-checkpoint.py
+++ ligotools/.ipynb_checkpoints/utils-checkpoint.py
@@ -24,7 +24,7 @@ def write_wavfile(filename,fs,data):
     wavfile.write(filename,int(fs), d)
 
 # function that shifts frequency of a band-passed signal
-def reqshift(data,fshift=100,sample_rate=4096):
+def reqshift(data, fshift =100, sample_rate = 4096):
     """Frequency shift the signal by constant
     """
     x = np.fft.rfft(data)
@@ -32,73 +32,78 @@ def reqshift(data,fshift=100,sample_rate=4096):
     df = 1.0/T
     nbins = int(fshift/df)
     # print T,df,nbins,x.real.shape
-    y = np.roll(x.real,nbins) + 1j*np.roll(x.imag,nbins)
-    y[0:nbins]=0.
+    y = np.roll(x.real, nbins) + 1j * np.roll(x.imag, nbins)
+    y[0:nbins] = 0.
     z = np.fft.irfft(y)
     return z
 
 import matplotlib.pyplot as plt
+import os
 
-def plot_matched_filter_results(time, timemax, tevent, eventname, det, SNR, 
-                                strain_whitenbp, template_match, template_fft,
-                                datafreq, data_psd, plottype='png'):
-    """
-    Plot matched filter results for a detector and save figures.
-    """
+def plot_matched_filter_results(time, timemax, tevent, eventname, det, SNR, strain_whitenbp,
+                                template_match, template_fft,
+                                datafreq, data_psd, plottype='png', make_dir='figures'):
+    os.makedirs(make_dir, exist_ok=True)
 
-    # -- Plot SNR vs time
+    pcolor = 'g' if det == 'L1' else 'r'
+    
+    # --- Plot SNR time series
     plt.figure(figsize=(10,8))
     plt.subplot(2,1,1)
-    plt.plot(time-timemax, SNR, 'r' if det=='H1' else 'g', label=f'{det} SNR(t)')
-    plt.grid('on')
+    plt.plot(time - timemax, SNR, pcolor, label=f'{det} SNR(t)')
+    plt.grid(True)
     plt.ylabel('SNR')
     plt.xlabel(f'Time since {timemax:.4f}')
     plt.legend(loc='upper left')
     plt.title(f'{det} matched filter SNR around event')
 
     plt.subplot(2,1,2)
-    plt.plot(time-timemax, SNR, 'r' if det=='H1' else 'g', label=f'{det} SNR(t)')
-    plt.grid('on')
+    plt.plot(time - timemax, SNR, pcolor, label=f'{det} SNR(t)')
+    plt.grid(True)
     plt.ylabel('SNR')
-    plt.xlim([-0.15,0.05])
+    plt.xlim([-0.15, 0.05])
     plt.xlabel(f'Time since {timemax:.4f}')
     plt.legend(loc='upper left')
-    plt.savefig(f'figures/{eventname}_{det}_SNR.{plottype}')
+    plt.savefig(f'{make_dir}/{eventname}_{det}_SNR.{plottype}')
+    plt.close()
 
-    # -- Plot whitened strain and template
+    # --- Plot whitened strain and template
     plt.figure(figsize=(10,8))
     plt.subplot(2,1,1)
-    plt.plot(time-tevent, strain_whitenbp, 'r' if det=='H1' else 'g', label=f'{det} whitened h(t)')
-    plt.plot(time-tevent, template_match, 'k', label='Template(t)')
+    plt.plot(time - tevent, strain_whitenbp, pcolor, label=f'{det} whitened h(t)')
+    plt.plot(time - tevent, template_match, 'k', label='Template(t)')
     plt.ylim([-10,10])
     plt.xlim([-0.15,0.05])
-    plt.grid('on')
+    plt.grid(True)
     plt.xlabel(f'Time since {timemax:.4f}')
-    plt.ylabel('Whitened strain (units of noise stdev)')
+    plt.ylabel('whitened strain (noise σ units)')
     plt.legend(loc='upper left')
     plt.title(f'{det} whitened data around event')
 
     plt.subplot(2,1,2)
-    plt.plot(time-tevent, strain_whitenbp-template_match, 'r' if det=='H1' else 'g', label=f'{det} resid')
+    plt.plot(time - tevent, strain_whitenbp - template_match, pcolor, label=f'{det} resid')
     plt.ylim([-10,10])
     plt.xlim([-0.15,0.05])
-    plt.grid('on')
+    plt.grid(True)
     plt.xlabel(f'Time since {timemax:.4f}')
-    plt.ylabel('Whitened strain (units of noise stdev)')
+    plt.ylabel('whitened strain (noise σ units)')
     plt.legend(loc='upper left')
-    plt.title(f'{det} Residual whitened data after subtracting template around event')
-    plt.savefig(f'figures/{eventname}_{det}_matchtime.{plottype}')
+    plt.title(f'{det} Residual whitened data after subtracting template')
+    plt.savefig(f'{make_dir}/{eventname}_{det}_matchtime.{plottype}')
+    plt.close()
 
-    # -- PSD and template plot
-    plt.figure(figsize=(10,6))
-    template_f = np.absolute(template_fft)*np.sqrt(np.abs(datafreq)) 
-    plt.loglog(datafreq, template_f, 'k', label='template(f)*sqrt(f)')
-    plt.loglog(datafreq, np.sqrt(data_psd), 'r' if det=='H1' else 'g', label=f'{det} ASD')
-    plt.xlim(20, datafreq.max())
+    # --- Plot PSD and template
+    # Use only the positive frequency part of the FFT
+    pos_freqs = datafreq[:len(data_psd)]
+    template_f = np.abs(template_fft[:len(pos_freqs)]) * np.sqrt(np.abs(pos_freqs))
+    plt.loglog(pos_freqs, template_f, 'k', label='template(f)*sqrt(f)')
+    plt.loglog(pos_freqs, np.sqrt(data_psd), pcolor, label=f'{det} ASD')
+    plt.xlim(20, 2048)
     plt.ylim(1e-24, 1e-20)
-    plt.grid()
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('strain noise ASD (strain/rtHz), template h(f)*rt(f)')
+    plt.grid(True)
+    plt.xlabel('frequency (Hz)')
+    plt.ylabel('strain noise ASD (strain/rtHz)')
     plt.legend(loc='upper left')
     plt.title(f'{det} ASD and template around event')
-    plt.savefig(f'figures/{eventname}_{det}_matchfreq.{plottype}')
+    plt.savefig(f'{make_dir}/{eventname}_{det}_matchfreq.{plottype}')
+    plt.close()
